880a5805bf9ba6c6949db42323740442e94ffbd5
Paula Colorado Ordoñez <you@example.com>
2020-08-28 01:42:32 -0500

    Pruebas Avance Req 6

--- App/app.py
+++ App/app.py
@@ -178,6 +178,9 @@ def ranking_genero(genero:str, num_peliculas:int, lst1:list, lst2:list,criterio:
             
         j = j + 1
     
+
+
+    
     if criterio == 1:
         busqueda = vote_count_lt
     if criterio == 2:
